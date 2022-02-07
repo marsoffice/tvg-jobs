5a7da7915fe6a25d7f8bdcf201e33ae87bb602fc
Alin Florin <alin@qoffice.ro>
2022-02-07 21:37:00 +0200

    -

---
+++ MarsOffice.Tvg.Jobs/Validators/JobValidator.cs
@@ -0,0 +1,14 @@
+using FluentValidation;
+using MarsOffice.Tvg.Jobs.Abstractions;
+
+namespace MarsOffice.Tvg.Jobs.Validators
+{
+    public class JobValidator : AbstractValidator<Job>
+    {
+        public JobValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Cron).NotEmpty();
+        }
+    }
+}
