81efb9185172d49bc499c50518a5c93ee90270f5
Alin Florin <alin@qoffice.ro>
2022-02-25 15:01:04 +0200

    f

--- MarsOffice.Tvg.Jobs/JobStarter.cs
+++ MarsOffice.Tvg.Jobs/JobStarter.cs
@@ -36,7 +36,7 @@ namespace MarsOffice.Tvg.Jobs
                     TableQuery.CombineFilters(
                         TableQuery.GenerateFilterCondition("Disabled", QueryComparisons.NotEqual, "true"),
                         TableOperators.And,
-                        $"not({TableQuery.GenerateFilterCondition("Cron", QueryComparisons.GreaterThan, "")})"
+                        TableQuery.GenerateFilterCondition("Cron", QueryComparisons.LessThanOrEqual, "")
                     )
                 );
 
